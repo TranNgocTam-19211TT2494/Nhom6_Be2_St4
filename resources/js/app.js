b2a67f9c0642426d6118a688d29fbbf2bdf28931
Long Kunz <cuvip1325@gmail.com>
2021-04-17 15:08:07 +0700

    add style & js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,1 @@
 require('./bootstrap');
-require('./jquery.slicknav');
-require('./main');
-require('./jquery-3.3.1.min');
-require('./jquery-ui.min');
-require('./jquery.nice-select.min');
-require('./mixitup.min');
-require('./owl.carousel.min');
