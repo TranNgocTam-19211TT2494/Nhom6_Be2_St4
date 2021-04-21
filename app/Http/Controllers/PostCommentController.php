d678a85873f786115d3650088bab04d83c27cb05
Long Kunz <cuvip1325@gmail.com>
2021-04-21 12:45:32 +0700

    Add controller

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Blog;
 
-class BlogController extends Controller
+class PostCommentController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,8 +13,7 @@ class BlogController extends Controller
      */
     public function index()
     {
-        $blogList = Blog::all();
-        return view('blog', compact('blogList'));
+        //
     }
 
     /**
